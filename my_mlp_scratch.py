3d5d91909644f69f14fe6a9f27eb1012124c89d1
iloveuxiu <1527817302@qq.com>
2019-07-24 15:40:02 +0800

    学会画图，要正确放置变量的位置

--- my_mlp_scratch.py
+++ my_mlp_scratch.py
@@ -1,5 +1,6 @@
 from mxnet.gluon import data as gdata
 from mxnet import nd, autograd
+import d2lzh as d2l
 
 #获取数据
 train_data = gdata.vision.FashionMNIST(train=True)
@@ -52,14 +53,16 @@ w1 = nd.random.normal(scale=0.01, shape=(num_inputs, num_hiddens))
 b1 = nd.zeros(num_hiddens)
 w2 = nd.random.normal(scale=0.01, shape=(num_hiddens, num_outputs))
 b2 = nd.zeros(num_outputs)
-epochs = 5
+epochs = 10
 lr = 0.1
 w1.attach_grad()
 w2.attach_grad()
 b1.attach_grad()
 b2.attach_grad()
+train_plot, test_plot = [], []
 for epoch in range(epochs):
     train_loss, n = 0.0, 0
+
     for feature, label in train_iter:
         with autograd.record():
             y_hat = net(feature, w1, w2, b1, b2)
@@ -68,8 +71,25 @@ for epoch in range(epochs):
         sgd([w1, w2, b1, b2], lr, batch_size)
         train_loss += l.asscalar()
         n += label.size
+    train_plot.append(train_loss/n)
     test_loss1 = test_loss(test_iter, net, w1, w2, b1, b2)
+    test_plot.append(test_loss1)
     print('epoch:%d, train_loss:%.3f, test_loss:%.3f' % (epoch + 1, train_loss/n, test_loss1))
+
+
+def semilogy(x_vals, y_vals, x_label, y_label, x2_vals=None, y2_vals=None,
+             legend=None, figsize=(3.5, 2.5)):
+    d2l.set_figsize(figsize)
+    d2l.plt.xlabel(x_label)
+    d2l.plt.ylabel(y_label)
+    d2l.plt.semilogy(x_vals, y_vals)
+    if x2_vals and y2_vals:
+        d2l.plt.semilogy(x2_vals, y2_vals, linestyle=':')
+        d2l.plt.legend(legend)
+        d2l.plt.show()
+
+semilogy(range(1, epochs + 1), train_plot, 'epochs', 'loss',
+         range(1, epochs + 1), test_plot, legend=['train', 'test'])
 
 
 
